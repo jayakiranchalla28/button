3cae08fe0f23ecb2dbe072e25a7712e384521782
wifin_imac <wifin_imac@IMAC.local>
2017-06-06 18:19:42 +0530

    Initial Commit

---
+++ nne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nne
+//
+//  Created by wifin_imac on 6/6/17.
+//  Copyright © 2017 wifin_imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nne
+//
+//  Created by wifin_imac on 6/6/17.
+//  Copyright © 2017 wifin_imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
